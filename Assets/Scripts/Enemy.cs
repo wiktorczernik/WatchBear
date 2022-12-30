1bd4740d02fddc1ac8ccbffb7f3a566b113c26ac
Wiktor Czernik <git@czernik.dev>
2022-12-30 03:50:03 +0100

    Basic enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,7 +9,6 @@ public class Enemy : MonoBehaviour
     public float playerMeetRange;
     public float playerFollowRange;
     public float attackRange;
-    public float attackRate;
     public float attackDamage;
     public float attackDelay;
     public float attackCooldown;
@@ -21,14 +20,35 @@ public class Enemy : MonoBehaviour
     {
         attackCooldown -= Time.deltaTime;
         if (attackCooldown < 0) attackCooldown = 0;
+
+
+        if (InAttackRange() && !HasCooldown())
+        {
+            Attack();
+        }
+        else
+        {
+            Move();
+        }
     }
     private void Move()
     {
-        throw new NotImplementedException();
+        Vector2 direction = transform.position - target.transform.position;
+        direction.Normalize();
+        transform.Translate(direction * Time.deltaTime);
     }
     private void Attack()
     {
         attackCooldown = attackDelay;
         target.Hurt(attackDamage);
+    }
+    private bool InAttackRange()
+    {
+        if (target == null) return false;
+        return Vector2.Distance(transform.position, target.transform.position) <= attackRange;
+    }
+    private bool HasCooldown()
+    {
+        return attackCooldown != 0;
     }
 }
