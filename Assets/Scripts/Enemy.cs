e5cad7f03cabe8bbec242e5cff9a40c16f518bf2
Wiktor Czernik <git@czernik.dev>
2022-12-30 03:37:36 +0100

    Enemy pt 2

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,29 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float moveSpeed;
+    public float playerMeetRange;
+    public float playerFollowRange;
+    public float attackRange;
+    public float attackRate;
+    public float attackDamage;
+    public float attackDelay;
+    public float attackCooldown;
 
-    private GameObject target;
+    LivingMixin target;
 
-    public void SetTarget(GameObject target)
+
+    private void Update()
     {
-
+        attackCooldown -= Time.deltaTime;
+        if (attackCooldown < 0) attackCooldown = 0;
+    }
+    private void Move()
+    {
+        throw new NotImplementedException();
+    }
+    private void Attack()
+    {
+        attackCooldown = attackDelay;
+        target.Hurt(attackDamage);
     }
 }
