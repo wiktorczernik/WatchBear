da69d63904b1ff043d6407dbc94c118d2e824c09
Wiktor Czernik <git@czernik.dev>
2022-12-30 14:12:59 +0100

    Bullet logic change

--- Assets/Scripts/Gunplay/Bullet.cs
+++ Assets/Scripts/Gunplay/Bullet.cs
@@ -1,14 +1,39 @@
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public BulletStats bullet;
+    public float timeInAir = 0.0f;
+    public bool dropped = false;
 
     private void Update()
     {
-        transform.Translate(transform.right * bullet.Speed * Time.deltaTime * Time.timeScale);
+        if (!dropped)
+        {
+            if (timeInAir < bullet.DropTime)
+            {
+                Fly();
+            }
+            else
+            {
+                Drop();
+            }
+        }
     }
-
+    public void Fly()
+    {
+        float timeFracture = timeInAir / bullet.DropTime;
+        float speedMultiply = bullet.DropSpeedCurve.Evaluate(timeFracture);
+        timeInAir += Time.deltaTime;
+        transform.Translate(transform.right * bullet.Speed * speedMultiply * Time.deltaTime * Time.timeScale);
+    }
+    public void Drop()
+    {
+        transform.rotation = Quaternion.Euler(Vector3.right);
+        timeInAir = 0.0f;
+        dropped = true;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out LivingMixin living) && bullet.isFriendly != living.isFriendly)
--- Assets/Scripts/Gunplay/BulletStats.cs
+++ Assets/Scripts/Gunplay/BulletStats.cs
@@ -6,6 +6,9 @@ public class BulletStats : ScriptableObject
     public float Damage = 10f;
     public float Speed = 7f;
 
+    public float DropTime;
+    public AnimationCurve DropSpeedCurve;
+
     public GameObject[] HitObjects;
 
     public bool isFriendly;
