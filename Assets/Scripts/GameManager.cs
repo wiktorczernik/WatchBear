ee330bfe1bc288d69063bdc442d6131ee812c68a
Wiktor Czernik <git@czernik.dev>
2022-12-31 15:20:00 +0100

    Game cycle (still needs fixes)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,11 +32,6 @@ public class GameManager : MonoBehaviour
         }
 
     }
-    private void OnEnable()
-    {
-        Debug.Log("OnBegin");
-        Begin();
-    }
 
     public void Begin()
     {
